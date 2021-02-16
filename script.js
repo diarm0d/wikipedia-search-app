21dd811e9a068cf785116dcb05a1f0444b0a95c4
diarm0d <diarmuid.odonovan@protonmail.com>
2021-02-16 12:38:05 +0000

    Added populating results functionality

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const searchBar = document.getElementById('searchbox');
+const searchResultsContainer = document.getElementById('search-results-container');
+const searchResultsWrapper = document.getElementById('search-results-wrapper');
 
 
 apiURl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
 proxyServer='https://obscure-sands-82167.herokuapp.com/';
 
-results = [];
+let resultsVisible = false;
+
+results = {};
 
 async function fetchWikiAPI(query) {
     try {
@@ -24,7 +28,33 @@ function getQuery(e) {
 }
 
 function populateResults(results) {
-    console.log(results);
+    resultsArray = Object.values(results);
+    // Create results container for each result in the array
+    resultsArray.forEach(val => {
+         title = val.title;
+         extract =  val.extract;
+         createElement(title, extract);
+      });
+      // Show results and jump to results section
+      searchResultsContainer.style.display = "flex";
+      window.location.hash = "search-results-container";
+}
+
+function createElement(title, extract) {
+    // Create result container
+    var resultContainer = document.createElement('div');
+    resultContainer.classList.add('result-container');
+    searchResultsWrapper.appendChild(resultContainer); 
+    // Add title & extract
+    var pageResult = document.createElement('span');
+    pageResult.classList.add('page-title');
+    pageResult.innerHTML = title;
+    resultContainer.appendChild(pageResult); 
+
+    var pageText = document.createElement('p');
+    pageText.classList.add('page-text');
+    pageText.innerHTML = extract;
+    resultContainer.appendChild(pageText); 
 }
 
 // Event listeners
